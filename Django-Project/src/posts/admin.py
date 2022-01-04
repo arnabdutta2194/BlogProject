24e40cdbffb35562d74a6079b096f5d910e3c63c
Arnab Dutta <arnab.dutta558.ad@gmail.com>
2022-01-04 16:31:20 +0530

    Adding updated changes

--- Django-Project/src/posts/admin.py
+++ Django-Project/src/posts/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-
+#Built In Admin Function to Register the Post Model into Admin Site
 admin.site.register(Post)
