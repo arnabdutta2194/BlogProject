412aa30633a5ca3c7bf50ff92e1b636a1aff81b8
Arnab Dutta <arnab.dutta558.ad@gmail.com>
2022-01-05 12:08:10 +0530

    Adding Posts Application

--- Blog/src/posts/admin.py
+++ Blog/src/posts/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Post
+# Register your models here.
 
-#Built In Admin Function to Register the Post Model into Admin Site
-admin.site.register(Post)
+#--- Model Admin Decides how we can display the Colums and helps us customize them in
+#--- Admin Page of Your Application
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title","updated","timestamp"] #-- Chooses which colums to display
+    # list_display_links = ["updated"] #-- To Change the Link to The Content
+    list_filter = ["updated","timestamp"] #-- To Give Filter Options
+    search_fields = ["title","content"] #-- Includes a Search field for mentioned Fields
+    class Meta:
+        model = Post
+
+admin.site.register(Post,PostModelAdmin)
