57c7c03d43170128b77e682bf35e218069f3836f
Arnab Dutta <arnab.dutta558.ad@gmail.com>
2022-01-06 18:21:58 +0530

    Commiting Updated Code

--- Blog/src/posts/views.py
+++ Blog/src/posts/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.db.models.query_utils import Q
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 
 # List All Posts View
 def post_list(request):
     # return HttpResponse("<h1>List</h1>")
     #--Rendering a Template : Parameters : Request -- Template Path -- Context
-    return render(request,"posts/index.html",{}) 
+    #-- Context is passed as a dictionary to HTML
+    querySet = Post.objects.all()
+    
+    # instance = Post.objects.get(id=6) #-- This will throw error is ID is not found
+    #-- Django provides a better way to handle this using get_object_or_404
+    # instance = get_object_or_404(Post,id=1)
+    # instance = get_object_or_404(Post,title = "FB Post")
+
+    if request.user.is_authenticated:
+        context = {
+            "title" : "User Is Authenticated",
+            "objects_list" : querySet
+        }
+    else:
+        context = {
+            "title" : "User Is Not Authenticated",
+            "objects_list" : querySet
+        }
+        
+    return render(request,"posts/index.html",context) 
 
 # Create A Post View
 def post_create(request):
@@ -14,7 +35,13 @@ def post_create(request):
 
 # Check Post Detail
 def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    instance = get_object_or_404(Post,title = "FB Post")
+    context = {
+        "title" : instance.title,
+        "instance" : instance,
+    }
+    return render(request,"posts/post_detail.html",context) 
+
 
 # Update Existing Post
 def post_update(request):
