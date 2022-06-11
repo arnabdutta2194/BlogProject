8bff9b7f61fd03ad1325946dbff1ec75c4ead1a8
Arnab Dutta <arnab.dutta558.ad@gmail.com>
2022-06-12 02:33:22 +0530

    Comments Thread and Reply

--- Blog/src/comments/models.py
+++ Blog/src/comments/models.py
@@ -1,15 +1,21 @@
+import re
 from tkinter import CASCADE
+from tkinter.messagebox import NO
 from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.urls import reverse
 # Create your models here.
 
 class CommentManager(models.Manager):
+    def all(self):
+        qs = super(CommentManager,self).filter(parent=None)
+        return qs
     def filter_by_instance(self,instance):
         content_type = ContentType.objects.get_for_model(instance.__class__)
         obj_id = instance.id
-        qs = super(CommentManager,self).filter(content_type=content_type,object_id = obj_id) #-- super(CommentManager,self) ==> Comments.objects
+        qs = super(CommentManager,self).filter(content_type=content_type,object_id = obj_id).filter(parent=None) #-- super(CommentManager,self) ==> Comments.objects
         return qs
 
 class Comment(models.Model):
@@ -20,11 +26,27 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
+    parent = models.ForeignKey("self",null=True,blank=True,on_delete=models.CASCADE) #--Parent Comment (Parent is top level and Children are Bottom Level)
     
     content= models.TextField(blank=False,null=False)
     timestamp =models.DateTimeField(auto_now_add=True)
 
     objects = CommentManager()
 
+    class Meta:
+        ordering = ["-timestamp"]
+        
     def __str__(self):
         return str(self.user.username)
+
+    def get_absolute_url(self):
+        return reverse("comments:comment_thread", kwargs={"com_id": self.id})
+
+    def children(self): #--Reply Elements (Replies)
+        return Comment.objects.filter(parent=self)
+    
+    @property
+    def is_parent(self):
+        if self.parent is not None:
+            return False
+        return True
--- Blog/src/comments/views.py
+++ Blog/src/comments/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .forms import CommentForm
+from .models import Comment
+from django.http import HttpResponseRedirect
+from django.contrib.contenttypes.models import ContentType
+from django.contrib import messages
 
 # Create your views here.
+def comment_thread(request,com_id):
+    obj = get_object_or_404(Comment, id=com_id)
+    content_object = obj.content_object #-- Post That the Comment is on
+    # content_id = obj.content_object.id
+    initial_data = {
+        "content_type" : content_object.get_content_type,
+        "object_id" :  obj.object_id
+    }
+    print(initial_data)
+    form = CommentForm(request.POST or None, initial=initial_data)
+
+    if form.is_valid():
+        print(form.cleaned_data)
+        # c_type = form.cleaned_data.get("content_type") 
+        # print(c_type)
+        content_type = ContentType.objects.get_for_model(content_object)
+        # content_type = ContentType.objects.get_for_model(model=c_type)
+        print(content_type)
+        obj_id = form.cleaned_data.get("object_id")
+        content_data = form.cleaned_data.get("content")
+        parent_obj = None
+        try:
+            parent_id = int(request.POST.get("parent_id")) #--- To get the Parent Comment ID from Request
+        except: 
+            parent_id = None
+        if parent_id:
+            parent_qs = Comment.objects.filter(id = parent_id)
+            if parent_qs.exists() and parent_qs.count() == 1:
+                parent_obj  = parent_qs.first()
+        print("parent_id",parent_id)
+        print("parent_obj",parent_obj)
+        new_comment, created = Comment.objects.get_or_create(
+            user = request.user,
+            content_type= content_type,
+            object_id = obj_id,
+            content = content_data,
+            parent = parent_obj
+        )
+
+        if created:
+            messages.success(request,"Comment Successfully Created")
+        print("Content Object")
+        print(new_comment.content_object)
+        return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
+
+    context ={
+        "comment" : obj,
+        "comment_form" : form
+    }
+    return render(request,"comment_thread.html", context)
